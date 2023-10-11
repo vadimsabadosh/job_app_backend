ee3c5df9dde0d7b130bec8e09a7154797d7f1258
Vadym Sabadosh <vadym.sabadosh@anvileight.com>
2023-10-11 21:49:48 +0300

    fix multiply adding bookmarks

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,10 +1,21 @@
 const Bookmark = require("../models/Bookmark");
+const mongoose = require("mongoose");
 const Job = require("../models/Job");
 
 module.exports = {
 	createBookmark: async (req, res) => {
 		try {
 			const jobId = req.body.job;
+
+			const result = await Bookmark.find({ userId: req.user.id }).populate(
+				"job"
+			);
+			const isExist = result.find((bkmrk) =>
+				bkmrk.job._id.equals(new mongoose.Types.ObjectId(jobId))
+			);
+			if (isExist) {
+				return res.status(401).json({ msg: "Bookmark is already in list" });
+			}
 			const job = await Job.findById(jobId);
 			if (!job) {
 				return res.status(404).json({ msg: "Job not found" });
